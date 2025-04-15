3ef489942d6df61703f409bf5ee6796f35433088
Andrws <rouzen2001@gmail.com>
2025-04-15 08:38:32 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jitCall-andresArcila',
+  webDir: 'www'
+};
+
+export default config;
